186b4a31ee538ae7b69d53d5277c7b0b9b5e1c89
Zhelezin <ilia.zhelezin@gmail.com>
2022-06-29 22:57:51 +0300

    Первые 4 программы

--- Program.cs
+++ Program.cs
@@ -1,1 +1,75 @@
-Task_01
+
+//Напишите программу, которая на вход
+//принимает два числа и выдаёт, какое число большее, а
+//какое меньшее.
+
+/* Console.WriteLine("Enter nunber:");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+
+Console.WriteLine(a);
+
+}
+else Console.WriteLine(b); */
+
+
+
+//Напишите программу, которая принимает на
+//вход три числа и выдаёт максимальное из этих чисел
+
+
+/* Console.WriteLine("Enter nunber:");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+
+if (a > b && a > c)
+
+{
+
+Console.WriteLine(a);
+}
+
+else if (b > a && b > c)
+{
+Console.WriteLine(b);
+}
+
+else 
+{
+Console.WriteLine(c);
+
+}
+ */
+
+ //Напишите программу, которая на вход
+//принимает число и выдаёт, является ли число чётным
+//(делится ли оно на два без остатка).
+
+/* Console.WriteLine("Enter nunber:");
+int a = int.Parse(Console.ReadLine());
+
+if (a %2 == 0)
+{
+
+Console.WriteLine("Yes");
+
+}
+else Console.WriteLine("No");  */
+
+//Напишите программу, которая на вход
+//принимает число (N), а на выходе показывает все чётные
+//числа от 1 до N
+
+/* Console.WriteLine("Enter nunber:");
+int a = int.Parse(Console.ReadLine());
+int i = 2;
+while (i <= a)
+{
+
+    Console.Write(i +  " "); 
+i+=2;
+} */
